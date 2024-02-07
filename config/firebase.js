1329c87dcb7cfe0dbc66084cb9f21712f3974d10
yorsh130204 <jorge130204@gmail.com>
2024-02-07 07:37:37 -0600

    Keys env

--- config/firebase.js
+++ config/firebase.js
@@ -6,13 +6,13 @@ import 'firebase/compat/database';
 const app = firebase.apps.length
   ? firebase.app()
   : firebase.initializeApp({
-      apiKey: "AIzaSyDAHKGxA7X7wN0TiP_hFzrIL_qAIbpEzqU",
-      authDomain: "serenitech-7a4b6.firebaseapp.com",
-      databaseURL: "https://serenitech-7a4b6-default-rtdb.firebaseio.com/",
-      projectId: "serenitech-7a4b6",
-      storageBucket: "serenitech-7a4b6.appspot.com",
-      messagingSenderId: "971151167264",
-      appId: "1:971151167264:web:f95914be5124b48b811145"
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
     });
 
 export const auth = app.auth();
